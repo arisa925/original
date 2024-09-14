25ddb1fc2731e1b4df3b42dc5a8bef4df6f0fd53
arisa925 <armr925d@gmail.com>
2024-09-15 06:52:21 +0900

    Add files via upload

---
+++ practice00/js script.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.a').on('click', function(){
+        $('.target').css('color','red');
+    });
+});
+
+$(function(){
+    $('#a').on('click', function(){
+        $('#target').css('color','yellow');
+    });
+});
